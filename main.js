5b067c176b72dcb20c88d74fb024a8383971c8ca
JAshleyCW <ashley.whitted@bcbsnc.com>
2018-08-09 00:11:12 -0400

    done for the night

---
+++ main.js
@@ -0,0 +1,10 @@
+var firstNumber = 3;
+var secondNumber = 7;
+var total = Number(firstNumber) + Number(secondNumber);
+alert (tatal);
+
+
+// set var for each button
+// click button and store value in an array -- var myArray []; empty array to push numbers to -- call index #
+// create a loop to 'loop' through each number to determine which ones to store in the array ??
+// array method forEach()
